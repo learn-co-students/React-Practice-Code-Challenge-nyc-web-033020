5016405252d33a9d562b22bae1e74afe6ef4d47d
Liz00000000000 <Elizabeth.Michael.Karen@gmail.com>
2020-06-09 13:12:53 -0400

    almost

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -7,11 +7,44 @@ const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushis: [ ],
+    firstSushiShown: 0, 
+    lastSushiShow: 4,
+    sushiEaten: []
+  }
+
+  componentDidMount(){
+    fetch(API)
+    .then(res => res.json())
+    .then(sushis => this.setState({ sushis }))
+  }
+
+  fourAtATime = (whereToStart, whereToStop) => {
+    const sushis = this.state.sushis.slice()
+    let showSushis = sushis.splice(whereToStart, whereToStop)
+    return showSushis
+  }
+
+  nextForSushis = () => {
+   this.setState({ firstSushiShown: this.state.firstSushiShown + 4 })  
+  }
+
+  eatSushi = (sushiId) => {
+    const sushisToChange = this.state.sushis.slice()
+    let eastenSushi = this.state.sushis.find(sushi => sushi.id === sushiId)
+    let index = this.state.sushis.indexOf(eastenSushi)
+    this.setState({ sushiEaten: [...this.state.sushiEaten, eastenSushi]})
+    eastenSushi.img_url = null 
+    sushisToChange[index] = eastenSushi
+    this.setState({ sushis: sushisToChange })
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer sushis={this.fourAtATime(this.state.firstSushiShown, this.state.lastSushiShow)} nextForSushis={this.nextForSushis} eatSushi={this.eatSushi}/>
+        <Table eatenSushis={this.state.sushiEaten}/>
       </div>
     );
   }
--- sushi-saga-client/src/containers/Table.js
+++ sushi-saga-client/src/containers/Table.js
@@ -1,17 +1,23 @@
 import React, { Fragment } from 'react'
 
 const Table = (props) => {
-
+   console.log(props)
   const renderPlates = (array) => {
     return array.map((x, index) => {
       return <div className="empty-plate" style={{ top: -7 * index }}/>
     })
   }
 
+  let moneyLeft = 120
+
+  let sushisEaten = props.eatenSushis.map(sushi => sushi.price)
+  
+  let totalPrice = sushisEaten.reduce((a,b) => a + b, 0)
+ 
   return (
     <Fragment>
       <h1 className="remaining">
-        You have: ${ /* Give me how much money I have left */ } remaining!
+        You have: ${moneyLeft - totalPrice} remaining!
       </h1>
       <div className="table">
         <div className="stack">
@@ -21,7 +27,7 @@ const Table = (props) => {
                and renders an empty plate
                for every element in the array
             */
-            renderPlates([])
+            renderPlates(props.eatenSushis)
           }
         </div>
       </div>

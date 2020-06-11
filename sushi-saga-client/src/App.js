16a1b050be1823b26e62c903f6cb748f7835985f
lightfire224 <elias.taveras@columbia.edu>
2020-06-11 08:38:27 -0400

    Done.

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -6,12 +6,68 @@ import Table from './containers/Table';
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
+  state = {
+    sushis: [],
+    displaySushis: [],
+    eatenSushis: [], //array of ids
+    x: 0,
+    y: 4,
+    tableMoney: 100
+  }
+
+  fetchAllSushis = () => {
+    fetch(API)
+      .then(r => r.json())
+      .then(sushis => {
+        this.setState({ sushis })
+        // this.setState({renderedSushis: sushis})
+      })
+  }
+
+  componentDidMount() {
+    this.fetchAllSushis()
+  }
+
+  renderNextSushis = () => {
+    this.setState({ x: this.state.x + 4 })
+    this.setState({ y: this.state.y + 4 })
+
+    this.setState({ displaySushis: this.state.sushis.slice(this.state.x, this.state.y) })
+  }
+
+  handleCount = () => {
+    this.setState({count: this.state.count + 1})
+  }
+
+  eatSushi = (id, price) => {
+    // finding the sushi to find its price, you could pass price in when eatSushi is invoked instead
+    // const eatenSushi = this.state.sushis.find(sushi => sushi.id === id);
+    console.log(price)
+
+    if(!this.state.eatenSushis.includes(id) /*&& this.state.tableMoney >= price*/ ){ //(this.state.tableMoney - price) >= 0
+      this.setState(prevState => ({ 
+        eatenSushis: [...prevState.eatenSushis, id],
+        tableMoney: prevState.tableMoney - price
+      }))
+    } else {
+      alert('you already ate that one')
+    }
+  }
 
   render() {
     return (
       <div className="app">
-        <SushiContainer />
-        <Table />
+        <SushiContainer
+          handleCount={this.handleCount}
+          sushis={this.state.displaySushis}
+          renderNextSushis={this.renderNextSushis}
+          eatenSushis={this.state.eatenSushis} 
+          eatSushi={this.eatSushi}/>          
+        />
+        <Table 
+          eatenSushis={this.state.eatenSushis}
+          tableMoney={this.state.tableMoney}
+        />
       </div>
     );
   }
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,16 +1,21 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
 
 const SushiContainer = (props) => {
   return (
     <Fragment>
       <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
+        {props.sushis.map(sushi => (
+        <Sushi 
+          handleCount={props.handleCount}
+          key={sushi.id} 
+          {...sushi}
+          eatenSushis={props.eatenSushis} 
+          eatSushi={props.eatSushi}
+        />
+        ))}
+        <MoreButton renderNextSushis={props.renderNextSushis} />
       </div>
     </Fragment>
   )
